c169aa6abe45f8d201b2d56cf744b0e2f7747548
aloan93 <a.loan93@gmail.com>
2024-08-04 00:19:34 +0100

    practising declaring getters and setters using the property decorator

--- bank_accounts.py
+++ bank_accounts.py
@@ -1,5 +1,17 @@
 class BankAccount:
     def __init__(self, initial_ammount, account_name):
         self.balance = initial_ammount
-        self.name = account_name
+        self.__name = account_name
         print(f"\nAccount '{self.name}' created\nBalance = £{self.balance:.2f}")
+    
+    @property
+    def name(self):
+        return(self.__name)
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            self.__name = new_name
+            print(f"\nName of account successfully changed to '{self.__name}'")
+        else:
+            print("\nRename failed\nAccount names must be of type 'string'")
--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from bank_accounts import *
 
 Jeff = BankAccount(500, "Jeff's nest egg")
 Holly = BankAccount(25, "Holly's change horde")
+
+print(f'\n{Jeff.name}')
+Jeff.name = 'This is a new name'
+print(f'\n{Jeff.name}')
+Jeff.name = 000
