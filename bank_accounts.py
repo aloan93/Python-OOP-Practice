64d15c54216735f4cd0a7fd8b7e2ced8f7d728ce
aloan93 <a.loan93@gmail.com>
2024-08-05 22:52:57 +0100

    implemented InterestAcc class and SavingsAcc class to practice inheritence and polymorphism

--- bank_accounts.py
+++ bank_accounts.py
@@ -1,50 +1,73 @@
 class BalanceException(Exception):
     pass
 
+
 class BankAccount:
-    def __init__(self, initial_ammount, account_name):
-        self.__balance = initial_ammount
-        self.__name = account_name
-        print(f"\nAccount '{self.name}' created\nBalance = £{self.__balance:.2f}")
+    def __init__(self, initial_amount, account_name):
+        self._balance = initial_amount
+        self._name = account_name
+        print(f"\nAccount '{self.name}' created\nBalance = £{self._balance:.2f}")
     
     @property
     def name(self):
-        return(self.__name)
+        return(self._name)
     
     @name.setter
     def name(self, new_name):
-        self.__name = new_name
-        print(f"\nName of account successfully changed to '{self.__name}'")
+        self._name = new_name
+        print(f"\nName of account successfully changed to '{self._name}'")
 
     def show_balance(self):
-        print(f"\nAccount '{self.__name}'\nBalance = £{self.__balance:.2f}")
+        print(f"\nAccount '{self._name}'\nBalance = £{self._balance:.2f}")
 
-    def deposit(self, ammount):
-        self.__balance += ammount
+    def deposit(self, amount):
+        self._balance += amount
         print(f"\nDeposit successful")
         self.show_balance()
 
-    def viable_transaction(self, ammount):
-        if self.__balance >= ammount:
+    def viable_transaction(self, amount):
+        if self._balance >= amount:
             return
         else:
-            raise BalanceException(f"\nAccount '{self.__name}' lacks suffcient funds - Current balance £{self.__balance:.2f}")
+            raise BalanceException(f"\nAccount '{self._name}' lacks suffcient funds - Current balance £{self._balance:.2f}")
         
-    def withdraw(self, ammount):
+    def withdraw(self, amount):
         try:
-            self.viable_transaction(ammount)
-            self.__balance -= ammount
+            self.viable_transaction(amount)
+            self._balance -= amount
             print("\nTransaction successful")
             self.show_balance()
         except BalanceException as error:
             print(f"\nTransaction failed: {error}")
 
-    def transfer(self, ammount, account):
+    def transfer(self, amount, account):
         try:
             print("\n**********\n\nBeginning Transfer...")
-            self.viable_transaction(ammount)
-            self.withdraw(ammount)
-            account.deposit(ammount)
+            self.viable_transaction(amount)
+            self.withdraw(amount)
+            account.deposit(amount)
             print("Transfer Complete!\n\n**********")
         except BalanceException as error:
             print(f"\nTransfer failed: {error}")
+
+
+class InterestRewardsAccount(BankAccount):
+    def deposit(self, amount):
+        self._balance += amount * 1.05
+        print(f"\nDeposit successful")
+        self.show_balance()
+
+
+class SavingsAccount(InterestRewardsAccount):
+    def __init__(self, initial_amount, account_name):
+        super().__init__(initial_amount, account_name)
+        self.fee = 5
+
+    def withdraw(self, amount):
+        try:
+            self.viable_transaction(amount + self.fee)
+            self._balance -= amount + self.fee
+            print("\nTransaction successful")
+            self.show_balance()
+        except BalanceException as error:
+            print(f"\nTransaction failed: {error}")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bank_accounts import *
 
-Jeff = BankAccount(500, "Jeff's nest egg")
+Jeff = InterestRewardsAccount(500, "Jeff's nest egg")
 Holly = BankAccount(25, "Holly's change horde")
 
 print(f'\n{Jeff.name}')
@@ -13,5 +13,13 @@ Holly.deposit(50)
 Holly.withdraw(50)
 Holly.withdraw(10.5)
 
+Jeff.deposit(100)
+
 Jeff.transfer(1000, Holly)
 Jeff.transfer(100, Holly)
+
+Charlie = SavingsAccount(100, "Charles' Student Debt")
+
+Charlie.withdraw(100)
+Charlie.withdraw(50)
+Charlie.deposit(50)
